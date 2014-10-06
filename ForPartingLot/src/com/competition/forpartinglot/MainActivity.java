b6b65e79f322ace2506350e7b3bb0486ba606fd6
thedevilking <thedevilking@qq.com>
2014-10-06 16:10:52 +0800

    添加地图

--- ForPartingLot/src/com/competition/forpartinglot/MainActivity.java
+++ ForPartingLot/src/com/competition/forpartinglot/MainActivity.java
@@ -1,15 +1,26 @@
 package com.competition.forpartinglot;
 
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.MapView;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
+	private MapView mMapView=null;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		//初始化百度地图
+		SDKInitializer.initialize(getApplicationContext());
 		setContentView(R.layout.activity_main);
+		
+		//获取地图控件引用  
+        mMapView = (MapView) findViewById(R.id.bmapView);  
 	}
 
 	@Override
@@ -18,5 +29,24 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
+	
+	@Override  
+    protected void onDestroy() {  
+        super.onDestroy();  
+        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理  
+        mMapView.onDestroy();  
+    }  
+    @Override  
+    protected void onResume() {  
+        super.onResume();  
+        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理  
+        mMapView.onResume();  
+        }  
+    @Override  
+    protected void onPause() {  
+        super.onPause();  
+        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理  
+        mMapView.onPause();  
+        }  
 
 }
